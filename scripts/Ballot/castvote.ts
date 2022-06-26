3bd00ba113e12384170f831ebdb48c222fb3410d
Efrain <efrainc.dev@gmail.com>
2022-06-26 07:18:45 -0500

    Eslint optimization

--- scripts/Ballot/castvote.ts
+++ scripts/Ballot/castvote.ts
@@ -1,4 +1,4 @@
-import { Contract, ethers,BigNumber, } from "ethers";
+import { Contract, ethers, BigNumber } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 // eslint-disable-next-line node/no-missing-import
@@ -9,48 +9,48 @@ import { Ballot } from "../../typechain";
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
-  async function main() {
-    const wallet =
-      process.env.MNEMONIC && process.env.MNEMONIC.length > 0
-        ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-        : new ethers.Wallet(process.env.PRIVATE_KEY!);
-    console.log(`Using address ${wallet.address}`);
-    const provider = new ethers.providers.InfuraProvider(
-      "ropsten",
-      process.env.INFURA_PROJECT_ID
-    );
-    const signer = wallet.connect(provider);
-    const balanceBN = await signer.getBalance();
-    const balance = Number(ethers.utils.formatEther(balanceBN));
-    console.log(`Wallet balance ${balance}`);
-    if (balance < 0.01) {
-      throw new Error("Not enough ether to perform these transaction");
-    }
-    if (process.argv.length < 3) throw new Error("Ballot address is missing");
-    const ballotAddress = process.argv[2];
-    if (process.argv.length < 4) throw new Error("Proposal number is missing");
-    const proposal = BigNumber.from(process.argv[3]);
-    console.log(
-      `Attaching ballot contract interface to address ${ballotAddress}`
-    );
-    const ballotContract: Ballot = new Contract(
-      ballotAddress,
-      ballotJson.abi,
-      signer
-    ) as Ballot;
-  
-    console.log(
-      `Cast a vote to proposal ${proposal.toString()} for Wallet ${
-        wallet.address
-      }`
-    );
-    const tx = await ballotContract.vote(proposal);
-    console.log("Awaiting for confirmations");
-    await tx.wait();
-    console.log(`Transaction completed. TX Hash is : ${tx.hash}`);
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY!);
+  console.log(`Using address ${wallet.address}`);
+  const provider = new ethers.providers.InfuraProvider(
+    "ropsten",
+    process.env.INFURA_PROJECT_ID
+  );
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether to perform these transaction");
   }
-  
-  main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+  if (process.argv.length < 3) throw new Error("Ballot address is missing");
+  const ballotAddress = process.argv[2];
+  if (process.argv.length < 4) throw new Error("Proposal number is missing");
+  const proposal = BigNumber.from(process.argv[3]);
+  console.log(
+    `Attaching ballot contract interface to address ${ballotAddress}`
+  );
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+  console.log(
+    `Cast a vote to proposal ${proposal.toString()} for Wallet ${
+      wallet.address
+    }`
+  );
+  const tx = await ballotContract.vote(proposal);
+  console.log("Awaiting for confirmations");
+  await tx.wait();
+  console.log(`Transaction completed. TX Hash is : ${tx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
